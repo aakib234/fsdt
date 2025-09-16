8cb47f5dccd9176f6ad5304780fe8882e31a1331
aakib <168260205+aakib234@users.noreply.github.com>
2025-09-16 12:53:29 +0530

    Add files via upload

---
+++ fsd.js
@@ -0,0 +1,7 @@
+let x = 5;
+let y = 10;
+console.log("sum of x and y is:",x+y);
+console.log("first java script");
+// Document.getEle("Message").innerHTML="hello sit";
+document.getElementById("Message").innerHTML="fghjk";
+
